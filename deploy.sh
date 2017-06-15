12899dc3a333ead07c5482a8d75ca0e432cf7408
Oliver.W <ole3021@gmail.com>
2017-06-15 17:23:06 +0800

    udpate docker iamges

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,12 @@
 ssh root@116.62.148.227 << EOF
 cd ~/NodeBB
 
-DATE=`date +%Y-%m-%d`
+git checkout master
 git pull
-# docker login --username=$1 --password=$2 registry.cn-hangzhou.aliyuncs.com
 
-# build and push to ali docker repo
-docker build -t registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb .
-# docker push registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb
+# build rew image
+docker build -t cnRustBB .
 
-# reanem to current and restart docker-compose
-docker rmi registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb:current || true
-echo "====================================cnRust======================================"
-echo ">>>>>>>>>>>>>>>>>>>> Updated docker image to 'current' tag. <<<<<<<<<<<<<<<<<<<<"
-echo "================================================================================"
-docker tag registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb:current
 echo "====================================cnRust======================================"
 echo ">>>>>>>>>>>>>>>>>>>>         DockerCompose Restarting...    <<<<<<<<<<<<<<<<<<<<"
 echo "================================================================================"
