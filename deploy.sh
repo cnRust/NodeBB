8e5eb7cd8478fc019f141491ea6839840d8bab14
Oliver.W <ole3021@gmail.com>
2017-06-15 17:20:23 +0800

    udpate deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ cd ~/NodeBB
 
 DATE=`date +%Y-%m-%d`
 git pull
-docker login --username=$1 --password=$2 registry.cn-hangzhou.aliyuncs.com
+# docker login --username=$1 --password=$2 registry.cn-hangzhou.aliyuncs.com
 
 # build and push to ali docker repo
 docker build -t registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb .
-docker push registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb
+# docker push registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb
 
 # reanem to current and restart docker-compose
 docker rmi registry.cn-hangzhou.aliyuncs.com/cn_rust/nodebb:current || true
