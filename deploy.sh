db7e5a18d5b09f3668b585548e5b893f6035dbd8
Oliver.W <ole3021@gmail.com>
2017-06-16 14:07:56 +0800

    udpate with https host

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker build -t rustbb .
 echo "====================================cnRust======================================"
 echo ">>>>>>>>>>>>>>>>>>>>         DockerCompose Restarting...    <<<<<<<<<<<<<<<<<<<<"
 echo "================================================================================"
-OSS_UPLOADS_BUCKET=cnrust OSS_UPLOADS_HOST=cdn.cnrust.org OSS_UPLOADS_PATH=/assets OSS_DEFAULT_REGION=oss-cn-hangzhou docker-compose up -d --no-deps --build
+OSS_UPLOADS_BUCKET=cnrust OSS_UPLOADS_HOST=https://cdn.cnrust.org OSS_UPLOADS_PATH=/assets OSS_DEFAULT_REGION=oss-cn-hangzhou docker-compose up -d --no-deps --build
 echo "====================================cnRust======================================"
 echo ">>>>>>>>>>>>>>>>>>>>           Clean up images.             <<<<<<<<<<<<<<<<<<<<"
 echo "================================================================================"
